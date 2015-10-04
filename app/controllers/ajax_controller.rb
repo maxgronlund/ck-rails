e67e4d5f335bafe6f8b8b8ee54661de132994795
Heru Joko <heru.mikami@gmail.com>
2015-10-04 20:55:01 +0800

    - payment search ajax

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -15,4 +15,45 @@ class AjaxController < ApplicationController
 
   end
 
+  def paybyid
+    ids = params[:ids]
+
+    # contoh nih buat API
+    # @pay = User.joins('LEFT OUTER JOIN jobs on users.id = jobs.user_id LEFT OUTER JOIN payments on jobs.id = payments.job_id')
+    # render :json => @pay.to_json(:include => { :jobs => { :include => :payment}})
+
+    paydata   = Payment.where(:payment_hash_id => ids).first
+    jobdata   = Job.find(paydata.job_id)
+    userdata  = User.find(jobdata.user_id)
+    str = "<tr>"
+    str += "<td>"+paydata.payment_hash_id+"</td>"
+    str += "<td class=\"time\">"+paydata.created_at.to_s+"</td>"
+    str += "<td>"+jobdata.job_name+"</td>"
+    str += "<td>"+userdata.user_name+"</td>"
+    str += "<td>"+paydata.payment_price.to_s+"</td>"
+    str += "<td>
+                            <div class=\"dropdown\">
+                              <button class=\"btn btn-white btn-sm dropdown-toggle\" type=\"button\" id=\"dropdownMenu2\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"true\">
+                                Action
+                                <span class=\"caret\"></span>
+                              </button>
+                              <ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenu2\">
+                                <li><a href=\"/admin/payments/"+paydata.payment_hash_id+"\">Details</a></li>
+                                <li class=\"\"><a href=\"/admin/payments/edit/"+paydata.payment_hash_id+"\">Accept</a></li>
+    <li><a href=\"/admin/payments/delete/"+paydata.payment_hash_id+"\">Decline</a></li>
+    </ul>
+                            </div>
+    </td>"
+    str +="</tr>"
+
+
+    render :json => str.to_json()
+
+  end
+
+  def paybydate
+    #params
+    start = params[:start]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get '/admin/payments' => 'payments#list'
 
   get '/ajax/citylist/:state' => 'ajax#citylist'
+  get '/ajax/payment/id/:ids' => 'ajax#paybyid'
 
   get '/db' => 'job#test'
 
