fd7c16da8dbf70f736c306a9d2b41fdbd08c84e9
Heru Joko <heru.mikami@gmail.com>
2015-10-27 14:08:00 +0800

    data load to chart company

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -6,7 +6,7 @@ class CompanyController < ApplicationController
   def dash
     @current = User.find(session[:user_id])
     @active = 'dashboard'
-
+    @timeline = JobSubscriptionRel.find_by_sql('select job_subscription_rels.created_at , count(job_subscription_rels.job_id) from job_subscription_rels inner join jobs on jobs.id = job_subscription_rels.job_id  where jobs.user_id = \''+session[:user_id].to_s+'\' group by job_subscription_rels.created_at')
   end
 
 end
